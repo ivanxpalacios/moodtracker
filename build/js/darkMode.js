c97f2fbd5dc6c19e8ac60af0a4b468e4017be9bb
Iván Palacios <ivanpalaciosdev@gmail.com>
2023-05-08 20:22:28 -0600

    Necessary changes to implement the darkMode button.

--- build/js/darkMode.js
+++ build/js/darkMode.js
@@ -14,4 +14,23 @@ function eventListeners() {
 // Modo oscuro
 function darkMode() {
     document.body.classList.toggle('dark-mode');
+
+    document.querySelector('.titulo').classList.toggle('text-white');
+
+    const links = document.querySelectorAll('.link-blanco');
+
+    links.forEach(link => {
+        link.classList.toggle('text-white');
+    });
+
+    document.querySelector('#navbar').classList.toggle('navbar-dark');
+
+    if(document.querySelector('#form').classList.contains('bg-white')) {
+        document.querySelector('#form').classList.add('blue');
+        document.querySelector('#form').classList.remove('bg-white');
+    } else {
+        document.querySelector('#form').classList.add('bg-white');
+        document.querySelector('#form').classList.remove('blue');
+    }
+
 }
