a1be24467e9530e212c2732ff11dedb260a468af
Iván Palacios <ivanpalaciosdev@gmail.com>
2023-05-10 12:14:50 -0600

    We added the file for editing entries

--- build/js/myEntries.js
+++ build/js/myEntries.js
@@ -1,16 +1,22 @@
 import { obtenerEntradas } from './API.js';
 
 (function() {
-    
+    // Variables
     const listadoEntradas = document.querySelector('#tbody');
 
+    // Eventos
     document.addEventListener('DOMContentLoaded', mostrarEntradas);
 
+    listadoEntradas.addEventListener('click', confirmarEliminar);
+
+
+
+    // Funciones
     async function mostrarEntradas () {
         const entradas = await obtenerEntradas();
 
         entradas.forEach(entrada => {
-            const {date, mood, thoughts, gratitude} = entrada;
+            const {date, mood, thoughts, gratitude, id} = entrada;
 
             const tr = document.createElement('tr');
 
@@ -21,16 +27,20 @@ import { obtenerEntradas } from './API.js';
                 <td>${gratitude}</td>
                 <td>
                     <div class="icono-contenedor">
-                        <img src="build/img/edit.svg" class="icono" />
-                        <img src="build/img/trash-solid.svg" class="icono" />
+                        <a class="icono" href="edit-entry.html?id=${id}"><img class="editar" src="build/img/edit.svg" /></a>
+                        <a class="icono" href="#" data-entrada="${id}"><img class="eliminar" src="build/img/trash-solid.svg"/> </a>
                     </div>
                 </td>
             `;
 
             listadoEntradas.appendChild(tr);
-
-
         });
+    }
+
+    function confirmarEliminar(e) {
+        if(e.target.classList.contains('eliminar')) {
+            console.log('Diste click en el botón de eliminar')
+        }
     }
 
 })();
